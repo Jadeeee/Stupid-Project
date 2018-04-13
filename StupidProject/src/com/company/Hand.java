97a858281df304e6f854d8e8276ca124d8d89ae6
Caelan Parker <cparker@students.inghamisd.org>
2018-04-13 13:40:56 -0400

    new

--- StupidProject/src/com/company/Hand.java
+++ StupidProject/src/com/company/Hand.java
@@ -4,4 +4,14 @@ package com.company;
  * Created by jburke on 3/26/2018.
  */
 public class Hand {
+
+    //list o cards
+
+    //constructor
+
+    //make method that adds card to hand
+
+    //get cards
+
+
 }
--- StupidProject/src/com/company/Player.java
+++ StupidProject/src/com/company/Player.java
@@ -4,5 +4,6 @@ package com.company;
  * Created by cparker on 3/22/2018.
  */
 public class Player {
-    //
+    private Hand playerHand;
+
 }
